63216f04c631803b25caf469458a56b27e1405ca
jmferreira1978 <jmferreira1978@72b49c86-8722-11dd-9205-735403a6da14>
2009-02-02 13:23:16 +0000

    Modificacion del MAIN para envio de parametros

--- TrabajoPracticoFinal/Implementacion/moacos/MOACO-FINAL/src/moacofinal/Main.java
+++ TrabajoPracticoFinal/Implementacion/moacos/MOACO-FINAL/src/moacofinal/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package moacofinal;
 
 /**
@@ -16,58 +15,76 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int problema; // indica el codigo del problema
         String parametros = new String(new char[100]); // indica el archivo de parametros a utilizar
-        int algoritmo; // algoritmo a ejecutar
         MOACO alg; // instancia del algoritmo
         Problem prob; // instancia del problema a resolver
         String cad = new String(new char[300]);
         String pr = new String(new char[60]);
-        String instancia = new String(new char[100]);
-        
 
-        problema = DefineConstantsSlave1.RC101;
-        prob = new VRPTW("d:\\instancias-parametros\\rc101.txt");
-        parametros = "d:\\instancias-parametros\\parametros_tsp.txt";
-        pr = "rc101.txt";
-        alg = new M3AS(prob, parametros);
+/*
+        String archivoProblema = "kroac100.tsp.txt";
+        String ruta = "d:\\instancias-parametros\\";
+        String archivoParametros = "parametros_tsp.txt";
+        String algoritmoEjecucion = "MOACS"; //Valores: MOACS, M3AS
+        String problemaEjecucion = "TSP"; //Valores: TSP, QAP, VRP
+*/
+        String archivoProblema = "kroac100.tsp.txt";
+        String ruta = "d:\\instancias-parametros\\";
+        String archivoParametros = "parametros_tsp.txt";
+        String algoritmoEjecucion = "MOACS"; //Valores: MOACS, M3AS
+        String problemaEjecucion = "TSP"; //Valores: TSP, QAP, VRP
+
+
+        parametros = ruta + archivoParametros;
+        pr = archivoProblema;
+
+        System.out.println();
+        System.out.println("*********Ejecutandose con los sgtes parametros*********");
+        System.out.println();
+        System.out.println("archivoProblema = " + archivoProblema);
+        System.out.println("ruta = " + ruta);
+        System.out.println("archivoParametros = " + archivoParametros);
+        System.out.println("algoritmoEjecucion = " + algoritmoEjecucion);
+        System.out.println("problemaEjecucion = " + problemaEjecucion);
+        System.out.println();
+
+        cad = "d:\\instancias-parametros\\PARETO-" + pr + "." + algoritmoEjecucion + "." + problemaEjecucion + ".pareto-" + System.currentTimeMillis();
 
         // Ejecutar el algoritmo indicado
-        if (problema == DefineConstantsSlave1.KROAB || problema == DefineConstantsSlave1.KROAC) {
+        if (problemaEjecucion.compareTo("TSP") == 0) {
+            prob = new TSP(ruta + archivoProblema);
+            if (algoritmoEjecucion.compareTo("MOACS") == 0) {
+                alg = new MOACS(prob, parametros);
+            } else {
+                alg = new M3AS(prob, parametros);
+            }
             alg.ejecutarTSP();
-        } else if (problema == DefineConstantsSlave1.QAP1 || problema == DefineConstantsSlave1.QAP2) {
+            alg.pareto.listarSoluciones(prob, cad);
+
+        } else if (problemaEjecucion.compareTo("QAP") == 0) {
+            prob = new TSP(ruta + archivoProblema);
+            if (algoritmoEjecucion.compareTo("MOACS") == 0) {
+                alg = new MOACS(prob, parametros);
+            } else {
+                alg = new M3AS(prob, parametros);
+            }
             alg.ejecutarQAP();
-        } else if (problema == DefineConstantsSlave1.C101 || problema == DefineConstantsSlave1.RC101) {
+            alg.pareto.listarSoluciones(prob, cad);
+
+        } else if (problemaEjecucion.compareTo("VRP") == 0) {
+            prob = new TSP(ruta + archivoProblema);
+            if (algoritmoEjecucion.compareTo("MOACS") == 0) {
+                alg = new MOACS(prob, parametros);
+            } else {
+                alg = new M3AS(prob, parametros);
+            }
             alg.ejecutarVRP();
+            alg.pareto.listarSolucionesVRP(prob, cad);
+
         }
-        cad="d:\\instancias-parametros\\"+pr+"."+".pareto";
-        alg.pareto.listarSolucionesVRP(prob, cad);
-        
+        System.out.println("Pareto Generado: " + cad);
+        System.out.println("Ejecución Finalizada...");
+
     }
-
 }
-final class DefineConstantsSlave1 {
 
-    public static final int PARETO = 50;
-    public static final String SLAVENAME = "slave";
-    public static final int CBIANT = 1;
-    public static final int CBIMC = 2;
-    public static final int CCOMP = 3;
-    public static final int CMAS = 4;
-    public static final int CM3AS = 5;
-    public static final int CMOACS = 6;
-    public static final int CMOAQ = 7;
-    public static final int CMOA = 8;
-    public static final int CPACO = 9;
-    public static final int KROAB = 10;
-    public static final int KROAC = 11;
-    public static final int QAP1 = 12;
-    public static final int QAP2 = 13;
-    public static final int C101 = 14;
-    public static final int RC101 = 15;
-    public static final int NTASKS = 2;
-    public static final int MSGINI = 20;
-    public static final int MSGDATA = 30;
-    public static final int MSGCONT = 40;
-    public static final int TERMINAR = 50;
-}
